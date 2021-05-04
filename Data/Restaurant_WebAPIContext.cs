5c863f5eee69eb503f8ae419eecb7eba195100a7
AVI KHEHRA <AVI KHEHRA@DESKTOP-5NAR473>
2021-05-05 11:29:57 +1200

    Add project files.

---
+++ Data/Restaurant_WebAPIContext.cs
@@ -0,0 +1,19 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Threading.Tasks;
+using Microsoft.EntityFrameworkCore;
+using Restaurant_WebAPI.Models;
+
+namespace Restaurant_WebAPI.Data
+{
+    public class Restaurant_WebAPIContext : DbContext
+    {
+        public Restaurant_WebAPIContext (DbContextOptions<Restaurant_WebAPIContext> options)
+            : base(options)
+        {
+        }
+
+        public DbSet<Restaurant_WebAPI.Models.Menu> Menu { get; set; }
+    }
+}
---
+++ Models/Menu.cs
@@ -0,0 +1,19 @@
+﻿using System;
+using System.Collections.Generic;
+using System.ComponentModel.DataAnnotations;
+using System.Linq;
+using System.Threading.Tasks;
+
+namespace Restaurant_WebAPI.Models
+{
+    public class Menu
+    {
+        [Key]
+        public int id { get; set; }
+        public string Dish_Name { get; set; }
+        public string Dietary_Restrictions { get; set; }
+        public int Rating { get; set; }
+        public int Price { get; set; }
+        
+    }
+}
